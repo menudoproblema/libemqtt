d6a7fdc6e00e26ef4de066dd549198602d9c8e45
Vicente Ruiz <vruiz2.0@gmail.com>
2012-04-03 14:04:36 +0200

    Full subscriptor example

--- client/pub.c
+++ client/pub.c
@@ -25,26 +25,37 @@
 #include <libemqtt.h>
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <linux/tcp.h>
 
 
+#define RCVBUFSIZE 1024
+uint8_t packet_buffer[RCVBUFSIZE];
+
 int socket_id;
+
+
+
 
 int send_packet(void* socket_info, const void* buf, unsigned int count)
 {
-	return write(*((int*)socket_info), buf, count);
+	int fd = *((int*)socket_info);
+	return send(fd, buf, count, 0);
 }
 
 int init_socket(mqtt_broker_handle_t* broker, const char* hostname, short port)
 {
+	int flag = 1;
+	int keepalive = 3; // Seconds
+
+	// Create the socket
 	if((socket_id = socket(PF_INET, SOCK_STREAM, 0)) < 0)
 		return -1;
 
-	// Prevent buffering
-	int flag = 1;
-	if (setsockopt(socket_id, IPPROTO_TCP, TCP_NODELAY, (char*) &flag, sizeof(flag)) < 0)
+	// Disable Nagle Algorithm
+	if (setsockopt(socket_id, IPPROTO_TCP, TCP_NODELAY, (char*)&flag, sizeof(flag)) < 0)
 		return -2;
 
 	struct sockaddr_in socket_address;
@@ -53,10 +64,12 @@ int init_socket(mqtt_broker_handle_t* broker, const char* hostname, short port)
 	socket_address.sin_port = htons(port);
 	socket_address.sin_addr.s_addr = inet_addr(hostname);
 
-	// Connect
+	// Connect the socket
 	if((connect(socket_id, (struct sockaddr*)&socket_address, sizeof(socket_address))) < 0)
 		return -1;
 
+	// MQTT stuffs
+	mqtt_set_alive(broker, keepalive);
 	broker->socket_info = (void*)&socket_id;
 	broker->send = send_packet;
 
@@ -65,22 +78,138 @@ int init_socket(mqtt_broker_handle_t* broker, const char* hostname, short port)
 
 int close_socket(mqtt_broker_handle_t* broker)
 {
-	return close(*(int*)broker->socket_info);
+	int fd = *((int*)broker->socket_info);
+	return close(fd);
 }
 
 
+
+
+int read_packet(int timeout)
+{
+	if(timeout > 0)
+	{
+		fd_set readfds;
+		struct timeval tmv;
+
+		// Initialize the file descriptor set
+		FD_ZERO (&readfds);
+		FD_SET (socket_id, &readfds);
+
+		// Initialize the timeout data structure
+		tmv.tv_sec = timeout;
+		tmv.tv_usec = 0;
+
+		// select returns 0 if timeout, 1 if input available, -1 if error
+		if(select(1, &readfds, NULL, NULL, &tmv))
+			return -2;
+	}
+
+	int total_bytes = 0, bytes_rcvd, packet_length;
+	memset(packet_buffer, 0, sizeof(packet_buffer));
+
+	while(total_bytes < 2) // Reading fixed header
+	{
+		if((bytes_rcvd = recv(socket_id, (packet_buffer+total_bytes), RCVBUFSIZE, 0)) <= 0)
+			return -1;
+		total_bytes += bytes_rcvd; // Keep tally of total bytes
+	}
+
+	packet_length = packet_buffer[1] + 2; // Remaining length + fixed header length
+
+	while(total_bytes < packet_length) // Reading the packet
+	{
+		if((bytes_rcvd = recv(socket_id, (packet_buffer+total_bytes), RCVBUFSIZE, 0)) <= 0)
+			return -1;
+		total_bytes += bytes_rcvd; // Keep tally of total bytes
+	}
+
+	return packet_length;
+}
+
+
+
+
+
+
+
 int main(int argc, char* argv[]) {
-	int result;
+	int packet_length;
+	uint16_t msg_id, msg_id_rcv;
 	mqtt_broker_handle_t broker;
 
 	mqtt_init(&broker, "libemqtt pub");
 	mqtt_init_auth(&broker, "username", NULL);
 	init_socket(&broker, "192.168.10.40", 1883);
 
-	result = mqtt_connect(&broker);
-	printf("Connect: %d\n", result);
+	// Connect
+	mqtt_connect(&broker);
+	packet_length = read_packet(1);
+	if(packet_length < 0)
+	{
+		fprintf(stderr, "Error(%d) on read packet!\n", packet_length);
+		return -1;
+	}
 
-	mqtt_publish(&broker, "hello/emqtt", "It's me", 0);
+	if(!MQTTMessageType(packet_buffer, MQTT_MSG_CONNACK))
+	{
+		fprintf(stderr, "CONNACK expected!\n");
+		return -2;
+	}
+
+	// Publish QoS 0
+	printf("Publish: QoS 0\n");
+	mqtt_publish(&broker, "hello/emqtt", "Example: QoS 0", 0);
+
+	// Publish QoS 1
+	printf("Publish: QoS 1\n");
+	mqtt_publish_with_qos(&broker, "hello/emqtt", "Example: QoS 1", 0, 1, &msg_id);
+	packet_length = read_packet(1);
+	if(packet_length < 0)
+	{
+		fprintf(stderr, "Error(%d) on read packet!\n", packet_length);
+		return -1;
+	}
+
+	if(!MQTTMessageType(packet_buffer, MQTT_MSG_PUBACK))
+	{
+		fprintf(stderr, "PUBACK expected!\n");
+		return -2;
+	}
+
+	msg_id_rcv = 0;
+	msg_id_rcv = packet_buffer[2]<<8;
+	msg_id_rcv |= packet_buffer[3];
+	if(msg_id != msg_id_rcv)
+	{
+		fprintf(stderr, "%d message id was expected, but %d message id was found!\n", msg_id, msg_id_rcv);
+		return -3;
+	}
+
+	// Publish QoS 2
+	printf("Publish: QoS 2\n");
+	mqtt_publish_with_qos(&broker, "hello/emqtt", "Example: QoS 2", 1, 2, &msg_id); // Retain
+	packet_length = read_packet(1);
+	if(packet_length < 0)
+	{
+		fprintf(stderr, "Error(%d) on read packet!\n", packet_length);
+		return -1;
+	}
+
+	if(!MQTTMessageType(packet_buffer, MQTT_MSG_PUBACK))
+	{
+		fprintf(stderr, "PUBACK expected!\n");
+		return -2;
+	}
+
+	msg_id_rcv = 0;
+	msg_id_rcv = packet_buffer[2]<<8;
+	msg_id_rcv |= packet_buffer[3];
+	if(msg_id != msg_id_rcv)
+	{
+		fprintf(stderr, "%d message id was expected, but %d message id was found!\n", msg_id, msg_id_rcv);
+		return -3;
+	}
 
 	mqtt_disconnect(&broker);
 	close_socket(&broker);
