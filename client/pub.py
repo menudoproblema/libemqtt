ef10d59faa500f5fa2749896608b2e9b4f506d71
Vicente Ruiz <vruiz2.0@gmail.com>
2012-04-12 13:54:42 +0200

    License

--- client/pub.py
+++ client/pub.py
@@ -1,3 +1,23 @@
+#
+# This file is part of python-emqtt.
+#
+# python-emqtt is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# python-emqtt is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with python-emqtt.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Copyright 2012 Vicente Ruiz Rodríguez <vruiz2.0@gmail.com>. All rights reserved.
+#
+
 from __future__ import print_function
 
 import select
